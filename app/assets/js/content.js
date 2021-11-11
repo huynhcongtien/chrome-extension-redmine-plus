20b649cc6b05673be4b728742499e28f48ff3e58
[Lampart] Huynh Cong Tien <cong_tien@lampart-vn.com>
2021-11-11 16:59:17 +0700

    Fix select2 search not auto focusing https://github.com/select2/select2/issues/5993#issuecomment-965559913

--- app/assets/js/content.js
+++ app/assets/js/content.js
@@ -887,6 +887,11 @@ $(function () {
         ;
     });
 
+    $(document).on('select2:open', () => {
+        let allFound = document.querySelectorAll('.select2-container--open .select2-search__field');
+        allFound[allFound.length - 1].focus();
+    });
+
     redminePlus.addClassForLabel();
 
     // change style of text for code
